7e693dcd81d370be9b8ec7024313e4b6063fd9ef
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-12 11:34:16 -0400

    feat: add function Timeout

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,4 +7,16 @@ function calc(num1, num2, callback) {
   return callback(num1, num2);
 }
 
-console.log(calc(2, 3, sum));
+console.log("Suma: " + calc(2, 3, sum));
+
+//-------------------------------------------------------
+setTimeout(function () {
+  console.log("JavaScript");
+}, 5000);
+
+//--------------------------------------------------------
+function grettin(name) {
+  console.log(`Hola ${name}`);
+}
+
+setTimeout(grettin, 2000, "Oscar");
