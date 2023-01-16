e5ca1137be65a87b6d14938e46063d157cd012d7
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-16 08:51:59 -0400

    feat: add function to display a single product

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -6,9 +6,22 @@ function fetchData(urlAPI) {
   return fetch(urlAPI);
 }
 
+// mostrando todos los productos
+// fetchData(`${API}/products`)
+//   .then((response) => response.json())
+//   .then((products) => {
+//     console.log(products);
+//   })
+//   .catch((error) => console.log(error));
+
+// Mostrando un producto en particular
 fetchData(`${API}/products`)
   .then((response) => response.json())
   .then((products) => {
-    console.log(products);
+    return fetchData(`${API}/products/${products[0].id}`)
+      .then((response) => response.json())
+      .then((product) => {
+        console.log(product);
+      });
   })
   .catch((error) => console.log(error));
