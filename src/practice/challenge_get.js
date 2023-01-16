c91296ba1f8eafad5e59136000cd2f97ef2ab6e4
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-16 10:22:37 -0400

    feat: add fetch of GET all

--- src/practice/challenge_get.js
+++ src/practice/challenge_get.js
@@ -1,3 +1,10 @@
 // Utilizamos la API de https://jsonplaceholder.typicode.com/
 import fetch from "node-fetch";
 const API = "https://jsonplaceholder.typicode.com";
+
+fetch(`${API}/comments`)
+  .then((response) => response.json())
+  .then((comments) => {
+    console.log("Todos los comentarios");
+    console.log(comments);
+  });
