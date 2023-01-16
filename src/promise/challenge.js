356aee003a02b6e0b1dafedeeefef329892a1605
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-16 08:41:05 -0400

    feat: add fetchData the products

---
+++ src/promise/challenge.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+const API = "https://api.escuelajs.co/api/v1";
+
+function fetchData(urlAPI) {
+  return fetch(urlAPI);
+}
+
+fetchData(`${API}/products`)
+  .then((response) => response.json())
+  .then((products) => {
+    console.log(products);
+  })
+  .catch((error) => console.log(error));
