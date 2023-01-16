fefe1ff65d6c7da87c541e6ae4be0376951feea7
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-16 09:00:27 -0400

    comentary: add comentary int function fetchData

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -17,20 +17,26 @@ function fetchData(urlAPI) {
 // Mostrando un producto en particular
 fetchData(`${API}/products`)
   .then((response) => response.json())
+  // obteniendo todos los products
   .then((products) => {
-    return fetchData(`${API}/products/${products[0].id}`)
-      .then((response) => response.json())
-      .then((product) => {
-        console.log("El producto seleccionado es: ");
-        console.log(product);
-        // obteniendo la categoria del producto
-        return fetchData(`${API}/categories/${product.category.id}`)
-          .then((response) => response.json())
-          .then((category) => {
-            console.log("La categoria del producto es: ");
-            console.log(category);
-          });
-      })
-      .catch((error) => console.log(error));
+    return (
+      fetchData(`${API}/products/${products[0].id}`)
+        .then((response) => response.json())
+        // obteniendo el primer producto
+        .then((product) => {
+          console.log("El producto seleccionado es: ");
+          console.log(product);
+          return (
+            fetchData(`${API}/categories/${product.category.id}`)
+              .then((response) => response.json())
+              // obteniendo la categoria del producto
+              .then((category) => {
+                console.log("La categoria del producto es: ");
+                console.log(category);
+              })
+          );
+        })
+        .catch((error) => console.log(error))
+    );
   })
   .catch((error) => console.log(error));
