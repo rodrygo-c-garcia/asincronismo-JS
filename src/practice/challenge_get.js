8809195e8148fbac024d3b8dcce3a0340f192bd8
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-16 10:39:54 -0400

    feat(challenge_post): add the function fetchData

--- src/practice/challenge_get.js
+++ src/practice/challenge_get.js
@@ -5,12 +5,14 @@ const API = "https://jsonplaceholder.typicode.com";
 fetch(`${API}/comments`)
   .then((response) => response.json())
   .then((comments) => {
+    // obteniendo todos los comentarios
     console.log("Todos los comentarios");
     console.log(comments);
 
     fetch(`${API}/comments/${comments[0].id}`)
       .then((response) => response.json())
       .then((coment) => {
+        // obteniendo un comentario en particular
         console.log("El comentario seleccionado es: ");
         console.log(coment);
       })
---
+++ src/practice/challenge_post.js
@@ -0,0 +1,13 @@
+import fetch from "node-fetch";
+const API = "https://jsonplaceholder.typicode.com";
+
+function postData(urlApi, data) {
+  const response = fetch(urlApi, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  return response;
+}
