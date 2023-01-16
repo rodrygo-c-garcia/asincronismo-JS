6ccfca6568c3c3625063f63a2bf4918ebc507c91
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-16 08:57:44 -0400

    feat: add function to show product category

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -21,7 +21,16 @@ fetchData(`${API}/products`)
     return fetchData(`${API}/products/${products[0].id}`)
       .then((response) => response.json())
       .then((product) => {
+        console.log("El producto seleccionado es: ");
         console.log(product);
-      });
+        // obteniendo la categoria del producto
+        return fetchData(`${API}/categories/${product.category.id}`)
+          .then((response) => response.json())
+          .then((category) => {
+            console.log("La categoria del producto es: ");
+            console.log(category);
+          });
+      })
+      .catch((error) => console.log(error));
   })
   .catch((error) => console.log(error));
